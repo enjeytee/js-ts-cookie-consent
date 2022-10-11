a2f9f01cbe17929975731a947c0db97e9579b2bb
enjeytee <enjeytee@gmail.com>
2022-10-12 03:32:38 +0800

    final version

--- index.ts
+++ index.ts
@@ -1,19 +1,44 @@
 const modal = document.querySelector(".modal") as HTMLDivElement;
 const modalCloseBtn = document.getElementById("modal-close-btn") as HTMLButtonElement;
-const loginForm = document.getElementById("consent-form") as HTMLFormElement;
+const consentForm = document.getElementById("consent-form") as HTMLFormElement;
 const modalText = document.getElementById("modal-text") as HTMLDivElement;
+const declineBtn = document.getElementById("decline-btn") as HTMLButtonElement;
+const modalChoiceBtns = document.getElementById("modal-choice-btns") as HTMLDivElement;
 setTimeout(() => {
     modal.style.display = "inline";
 }, 1500);
 modalCloseBtn.addEventListener("click", () => {
     modal.style.display = "none";
 });
-loginForm.addEventListener("submit", event => {
+consentForm.addEventListener("submit", event => {
     event.preventDefault();
-    // alert("Form sumbitted!")
     modalText.innerHTML = `
-        <div class="modal-inner-loading">
-            <img src="
-        </div>
+    <div class="modal-inner-loading">
+        <img src="./images/loading.svg"/>
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>
     `
+    const consentFormData = new FormData(consentForm);
+    const fullName = consentFormData.get("fullName");
+    const email = consentFormData.get("email");
+    setTimeout(() => {
+        const uploadText = document.getElementById("uploadText") as HTMLParagraphElement;
+        uploadText.innerText = "Making the sale..."
+        setTimeout(() => {
+            const modalInner = document.getElementById("modal-inner") as HTMLDivElement;
+            modalInner.innerHTML = `
+                <h2>Thanks <span class="modal-display-name">${fullName}</span> you sucker!</h2>
+                <p>We just sold the rights to your eternal soul.</p>
+                <div class="idiot-gif">
+                    <img src="./images/pirate.gif">
+                </div>
+            `
+            modalCloseBtn.disabled = false;
+        }, 1500);
+    }, 1500);
+});
+declineBtn.addEventListener("mouseover", () => {
+    modalChoiceBtns.classList.toggle("reverse");
 });
--- out/index.js
+++ out/index.js
@@ -1,21 +1,46 @@
 "use strict";
 const modal = document.querySelector(".modal");
 const modalCloseBtn = document.getElementById("modal-close-btn");
-const loginForm = document.getElementById("consent-form");
+const consentForm = document.getElementById("consent-form");
 const modalText = document.getElementById("modal-text");
+const declineBtn = document.getElementById("decline-btn");
+const modalChoiceBtns = document.getElementById("modal-choice-btns");
 setTimeout(() => {
     modal.style.display = "inline";
 }, 1500);
 modalCloseBtn.addEventListener("click", () => {
     modal.style.display = "none";
 });
-loginForm.addEventListener("submit", event => {
+consentForm.addEventListener("submit", event => {
     event.preventDefault();
-    // alert("Form sumbitted!")
     modalText.innerHTML = `
-        <div class="modal-inner-loading">
-            <img src="
-        </div>
+    <div class="modal-inner-loading">
+        <img src="./images/loading.svg"/>
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>
     `;
+    const consentFormData = new FormData(consentForm);
+    const fullName = consentFormData.get("fullName");
+    const email = consentFormData.get("email");
+    setTimeout(() => {
+        const uploadText = document.getElementById("uploadText");
+        uploadText.innerText = "Making the sale...";
+        setTimeout(() => {
+            const modalInner = document.getElementById("modal-inner");
+            modalInner.innerHTML = `
+                <h2>Thanks <span class="modal-display-name">${fullName}</span> you sucker!</h2>
+                <p>We just sold the rights to your eternal soul.</p>
+                <div class="idiot-gif">
+                    <img src="./images/pirate.gif">
+                </div>
+            `;
+            modalCloseBtn.disabled = false;
+        }, 1500);
+    }, 1500);
+});
+declineBtn.addEventListener("mouseover", () => {
+    modalChoiceBtns.classList.toggle("reverse");
 });
 //# sourceMappingURL=index.js.map
