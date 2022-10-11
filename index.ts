ae26899fcdd64e8c5175391d08286381be96a7b8
enjeytee <enjeytee@gmail.com>
2022-10-12 01:07:48 +0800

    update

--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
 const modal = document.querySelector(".modal") as HTMLDivElement;
-const closeBtn = document.getElementById("modal-close-btn") as HTMLButtonElement;
+const modalCloseBtn = document.getElementById("modal-close-btn") as HTMLButtonElement;
+const loginForm = document.getElementById("consent-form") as HTMLFormElement;
+const modalText = document.getElementById("modal-text") as HTMLDivElement;
 setTimeout(() => {
     modal.style.display = "inline";
 }, 1500);
-closeBtn.addEventListener("click", () => {
+modalCloseBtn.addEventListener("click", () => {
     modal.style.display = "none";
+});
+loginForm.addEventListener("submit", event => {
+    event.preventDefault();
+    // alert("Form sumbitted!")
+    modalText.innerHTML = `
+        <div class="modal-inner-loading">
+            <img src="
+        </div>
+    `
 });
--- out/index.js
+++ out/index.js
@@ -1,10 +1,21 @@
 "use strict";
 const modal = document.querySelector(".modal");
-const closeBtn = document.getElementById("modal-close-btn");
+const modalCloseBtn = document.getElementById("modal-close-btn");
+const loginForm = document.getElementById("consent-form");
+const modalText = document.getElementById("modal-text");
 setTimeout(() => {
     modal.style.display = "inline";
 }, 1500);
-closeBtn.addEventListener("click", () => {
+modalCloseBtn.addEventListener("click", () => {
     modal.style.display = "none";
+});
+loginForm.addEventListener("submit", event => {
+    event.preventDefault();
+    // alert("Form sumbitted!")
+    modalText.innerHTML = `
+        <div class="modal-inner-loading">
+            <img src="
+        </div>
+    `;
 });
 //# sourceMappingURL=index.js.map
